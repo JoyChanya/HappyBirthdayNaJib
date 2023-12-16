dcd0b982e2b7167b294d8e9c2b944b4e6f5589d9
JoyChanya <chanya.joy23@gmail.com>
2023-12-16 20:10:16 +0700

    change img

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v1uZLK6zbzjWxvPf6ZTNardz7Jdf9haX1wyhzFblp1s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fOf6k8Nvmo31t6ZeElSq+F0liJFZKKH57m/CxzgGwis=\"\n}"
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'HappyBirthdayNaJib',
+  title: 'HappyBirthdayDearNaJib',
   description: 'Webpage for NaJibs 2023 Birthday',
 }
 
